4a547e6ea79354e06035a92c09bdc3968d03b3eb
mohammad hmedat <humedat23@gmail.com>
2025-10-24 19:15:49 +0300

    style: build the initail architechure for e-commerce project

---
+++ lib/views/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(title),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
