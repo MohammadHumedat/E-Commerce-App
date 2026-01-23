2d77adb6c05e58ed35a18a145dd3f9636a5f8009
mohammad hmedat <humedat23@gmail.com>
2026-01-24 01:04:38 +0300

    feat: Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:e_commerce_app/Constants/app_routes.dart';
+import 'package:e_commerce_app/firebase_options.dart';
 import 'package:e_commerce_app/utils/app_router.dart';
-
-import 'package:e_commerce_app/views/pages/main_scaffold.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      FlutterNativeSplash.remove();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +39,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-
       initialRoute: AppRoutes.login,
     );
   }
